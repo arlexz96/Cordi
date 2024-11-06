a29cec8b0cb868ab073c5462fd463711bdb8fd7b
arlexz96 <arlex.z96@gmail.com>
2024-11-06 09:58:19 -0500

    Firts Commit

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EventRequest;
+use App\Models\Event;
+use App\Models\EventCapacity;
+use App\Models\Location;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function Index()
+    {
+        // Obtenemos todas las categorías
+        $events = Event::all();
+        // Pasamos las categorías a la vista 'categories.index'
+        return view('events.index', compact('events'));
+    }
+
+    public function adminIndex()
+    {
+        // Obtenemos todas las categorías
+        $events = Event::all();
+        // Pasamos las categorías a la vista 'categories.index'
+        return view('events.adminIndex', compact('events'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('events.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(EventRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $location = Location::create([
+            'country' => $request->input('country'),
+            'city' => $request->input('city')
+        ]);
+
+        $eventCapacity = EventCapacity::create([
+            'max_people' => $request->input('max_people'),
+            'places_availables' => $request->input(('max_people'))
+        ]);
+
+        /* Event::create($validatedData); */
+        Event::create(array_merge($validatedData, [
+            'location_id' => $location->id,
+            'event_capacity_id' =>$eventCapacity->id,
+        ]));
+
+        return redirect()->route('events.index')->with('success', 'Categoría creada con éxito.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $event = Event::findOrFail($id);
+        // Mostramos la vista de detalles con la categoría específica
+        return view('events.show', compact('event'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        // Obtenemos la categoría por ID
+        $event = Event::findOrFail($id);
+        // Retornamos la vista para editar la categoría
+        return view('events.edit', compact('event'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(EventRequest $request, string $id)
+    {
+        $validatedData = $request->validated();
+
+        $event = Event::findOrFail($id);
+        $event->location->update($validatedData);
+        $event->eventCapacity->update($validatedData);
+        $event->update($validatedData);
+
+        return redirect()->route('events.adminIndex')->with('success', 'Categoría actualizada exitosamente.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        // Verifica si el usuario está autenticado y es administrador
+        /* if (!Auth::check() || Auth::user()->role_id != 1) { // 1 corresponde al rol de 'admin'
+            return redirect()->route('categories.index')->with('error', 'No tienes permiso para acceder a esta sección.');
+        } */
+        // Buscamos la categoría por ID y la eliminamos
+        $event = Event::findOrFail($id);
+        $event->delete();
+
+        // Redirigimos a la lista de categorías con un mensaje de éxito
+        return redirect()->route('events.adminIndex')->with('success', 'Categoría eliminada con éxito.');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\EventController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Ruta option 1
+Route::get('adminIndex', [EventController::class, 'adminIndex'])->name('events.adminIndex');
+Route::get('events/create', [EventController::class, 'create'])->name('events.create');
+Route::post('events', [EventController::class, 'store'])->name('events.store');
+Route::get('events/{id}', [EventController::class, 'show'])->name('events.show');
+Route::get('events/{id}/edit', [EventController::class, 'edit'])->name('events.edit');
+Route::put('events/{id}', [EventController::class, 'update'])->name('events.update');
+Route::delete('events/{id}', [EventController::class, 'destroy'])->name('events.destroy');
+Route::get('index', [EventController::class, 'index'])->name('events.index');
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
